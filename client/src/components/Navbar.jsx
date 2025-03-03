3c802bfbbf200332a124f314b4b85c7db41adf8a
dpeyupkaya <dpeyupkaya@gmail.com>
2025-03-03 23:06:37 +0300

    .

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -19,14 +19,14 @@ export const Navbar = () => {
 
         {/* Desktop Menu */}
         <ul className="hidden md:flex space-x-6">
-          <li><a href="#" className="text-white hover:text-gray-200">Hesap İşlemleri</a></li>
+          <li><a href="#" className="text-white hover:text-gray-200">Hesap  İşlemleri</a></li>
           <li><a href="#" className="text-white hover:text-gray-200">Para Transferi</a></li>
           <li><a href="#" className="text-white hover:text-gray-200">Ödemeler</a></li>
           <li><a href="#" className="text-white hover:text-gray-200">Kartlar</a></li>
           <li><a href="#" className="text-white hover:text-gray-200">Daha Fazla</a></li>
         </ul>
 
-        {/* Mobile Menu Button */}
+       
         <button onClick={toggleMobileMenu} className="md:hidden text-white focus:outline-none">
           <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" d="M4 6h16M4 12h16m-7 6h7"></path>
@@ -34,7 +34,7 @@ export const Navbar = () => {
         </button>
       </div>
 
-      {/* Mobile Menu */}
+   
       <div className={`fixed inset-y-0 left-0 w-64 bg-orange-500 transform ${isMobileMenuOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out md:hidden`}>
         <div className="p-4">
        
