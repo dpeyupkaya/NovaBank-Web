d0f905475a5168e99049c2b8c820fea44210a6b3
dpeyupkaya <dpeyupkaya@gmail.com>
2025-04-30 14:20:04 +0300

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react'; // Doğru import
 import { Navbar } from './components/Navbar';
-import { Router, Routes, Route } from 'react-router-dom';
+import { Router, Routes, Route, BrowserRouter } from 'react-router-dom';
 import { HesapIslemler } from './components/HesapIslemler'; 
 import { Home } from './components/Home';
 
 const App = () => {
   return (
-    <Router>
+    <BrowserRouter.Router>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path="/hesap-islemleri" element={<HesapIslemler />} />
       </Routes>
-    </Router>
+    </BrowserRouter.Router>
   );
 };
 
